fe473542d47eb6dfaf8d0f012360948c3a45a0af
abhinavgautam01 <abgautam1017@gmail.com>
2025-02-11 22:41:59 +0530

    finalizing-integration

--- project8-taskList/todo-react/src/components/task/_taskFooter.tsx
+++ project8-taskList/todo-react/src/components/task/_taskFooter.tsx
@@ -2,13 +2,14 @@ import React, { FC, ReactElement } from "react";
 import { Switch, FormControlLabel, Box, Button } from "@mui/material";
 import { ITaskFooter } from "./interfaces/ITaskFooter";
 import PropTypes from "prop-types";
+import { Status } from "../createTaskForm/enums/Status";
 
 export const TaskFooter: FC<ITaskFooter> = (props): ReactElement=>{
     const {id, status, onStatusChange = (e)=>console.log(e), onClick = (e)=>console.log(e)}=props
     return(
         <Box display="flex" justifyContent="space-between" alignItems="center" mt={4}>
-            <FormControlLabel label="In Progress" control={<Switch onChange={(e)=>onStatusChange(e)} color="warning"/>}></FormControlLabel>
-            <Button variant="contained" color="success" size="small" sx={{ color: '#ffffff' }} onClick={(e)=>onClick(e)}>Mark Complete</Button>
+            <FormControlLabel label="In Progress" control={<Switch defaultChecked={status === Status.inProgress} onChange={(e)=>onStatusChange(e, id)} color="warning"/>}></FormControlLabel>
+            <Button variant="contained" color="success" size="small" sx={{ color: '#ffffff' }} onClick={(e)=>onClick(e, id)}>Mark Complete</Button>
         </Box>
     )
 }
--- project8-taskList/todo-react/src/components/taskArea/taskArea.tsx
+++ project8-taskList/todo-react/src/components/taskArea/taskArea.tsx
@@ -1,11 +1,14 @@
 import React, {FC, ReactElement} from "react";
-import {Grid, Box} from "@mui/material"
+import {Grid, Box, Alert, LinearProgress} from "@mui/material"
 import format from "date-fns/format";
 import { TaskCounter } from "../taskCounter/taskCounter";
 import { Task } from "../task/task";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import { sendApiRequest } from "../../helpers/sendApiRequest";
 import { ITaskApi } from "./interfaces/ITaskApi";
+import { Status } from "../createTaskForm/enums/Status";
+import { IUpdateTask } from "../createTaskForm/interfaces/IUpdateTask";
+import { countTasks } from "./helpers/countTasks";
 
 export const TaskArea: FC = (): ReactElement =>{
     
@@ -13,7 +16,17 @@ export const TaskArea: FC = (): ReactElement =>{
         queryKey: ['tasks'], 
         queryFn: async () => await sendApiRequest<ITaskApi[]>('http://localhost:3200/tasks', 'GET')
       });
-      
+
+    const updateTaskMutation = useMutation({mutationFn: (data: IUpdateTask) => sendApiRequest('http://localhost:3200/tasks', 'PUT', data)});
+    
+    function onStatusChangeHandler(e: React.ChangeEvent<HTMLInputElement>, id: string){
+        updateTaskMutation.mutate({id, status: e.target.checked ? Status.inProgress: Status.todo})
+    }
+
+    function markCompleteHandler(e: React.MouseEvent<HTMLButtonElement> | React.MouseEvent<HTMLAnchorElement>, id: string){
+        updateTaskMutation.mutate({id, status: Status.completed})
+    }
+
     return (
         <Grid item md={8} px={4}>
             <Box mb={8} px={4}>
@@ -21,14 +34,22 @@ export const TaskArea: FC = (): ReactElement =>{
             </Box>
             <Grid container display="flex" justifyContent="center">
                 <Grid item display="flex" flexDirection="row" justifyContent="space-around" alignItems="center" md={10} xs={12} mb={8}>
-                    <TaskCounter/>
-                    <TaskCounter/>
-                    <TaskCounter/>
+                    <TaskCounter count={data?countTasks(data, Status.todo):undefined} status={Status.todo}/>
+                    <TaskCounter count={data?countTasks(data, Status.inProgress):undefined} status={Status.inProgress}/>
+                    <TaskCounter count={data?countTasks(data, Status.completed):undefined} status={Status.completed}/>
                 </Grid>
                 <Grid item display="flex" flexDirection="column" xs={10} md={8}>
-                    <Task/>
-                    <Task/>
-                    <Task/>
+                    <>
+                    {error && (<Alert severity="error">
+                        There was an error fetching your tasks
+                    </Alert>)}
+
+                    {!error && Array.isArray(data) && data.length === 0 && <Alert severity="warning">
+                        You do not have any tasks created yet. Start by creating some tasks
+                    </Alert>}
+                    {isPending ? <LinearProgress/>: (Array.isArray(data) && data.length>0 && data.map((each, index)=>{return each.status===Status.todo || each.status===Status.inProgress ? (<Task key={index + each.priority} id={each.id} title={each.title} date={new Date(each.date)} description={each.description} priority={each.priority} status={each.status} onStatusChange={onStatusChangeHandler} onClick={markCompleteHandler} />) : (false)}) )}
+
+                    </>
                 </Grid>
             </Grid>
         </Grid>
